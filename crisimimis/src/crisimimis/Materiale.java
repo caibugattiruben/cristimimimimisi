65071a0cb1f75bd909aae070b5a23ca7319a432e
ruben <caibugatti.ruben@franchettisalviani.net>
2025-12-02 13:27:02 +0100

    ho quasi messo tutto

---
+++ crisimimis/src/crisimimis/Materiale.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crisimimis;
+
+/**
+ *
+ * @author caibugatti.ruben
+ */
+public enum Materiale {
+    PLASTICA,
+    LEGNO,
+    FERRO,
+    CRISTALLODINEVE,
+    FRAMMENTOMAGICO;
+}
